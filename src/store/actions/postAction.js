919fedfd7d227b3e4df03202937bfc2f94a108e5
Karthic <karthics004@gmail.com>
2025-09-25 18:19:23 +0530

    Added fetchPost

--- src/store/actions/postAction.js
+++ src/store/actions/postAction.js
@@ -1,14 +1,26 @@
-export const fetchPostRequest = () => ({
+export const fetchPostsRequest = () => ({
     type:"FETCH_POSTS_REQUEST",
 });
 
-export const fetchPostSuccess = (posts) => ({
+export const fetchPostsSuccess = (posts) => ({
     type:"FETCH_POSTS_SUCCESS",
     payload: posts,
 });
 
-export const fetchPostFailure = (error) => ({
+export const fetchPostsFailure = (error) => ({
     type:"FETCH_POSTS_FAILURE",
     payload: error,
 });
 
+export const fetchPost = () => async (dispatch) =>{
+    dispatch(fetchPostsRequest());
+
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        dispatch(fetchPostsSuccess());
+    }catch(error){
+        dispatch(fetchPostsFailure());
+
+    }
+};
